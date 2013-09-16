5dfa5fcf3007a24009a6eab73b8ddaac12413522
Carl Schwope <schwope.carl@gmail.com>
2013-09-16 10:12:59 -0400

    setup validation for existence, do migration

--- spec/models/parking_registration_spec.rb
+++ spec/models/parking_registration_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe ParkingRegistration do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_valid( :email ).when('user@example.com') }
+  it { should_not have_valid( :email).when( nil, '', 'foo' ) }
+
+  it { should validate_presence_of( :first_name ) }
+  it { should validate_presence_of( :last_name  ) }
+
+  it { should     have_valid( :spot_number ).when( 5, 20 )      }
+  it { should_not have_valid( :spot_number ).when( nil, 0, 61 ) }
+
+  it { should     have_valid( :parked_on ).when( Date.today ) }
+  it { should_not have_valid( :parked_on ).when( nil, '' )    }
+
 end

6e136e53db185789586e7923bc70d5a6fee113cc
Carl Schwope <schwope.carl@gmail.com>
2013-09-16 14:29:31 -0400

    enough design

--- app/models/parking_registration.rb
+++ app/models/parking_registration.rb
@@ -2,6 +2,8 @@ class ParkingRegistration < ActiveRecord::Base
 
   validates_presence_of :first_name
   validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :spot_number
   validates_presence_of :parked_on
 
   validates_format_of :email, with: /[A-Z0-9.&+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i

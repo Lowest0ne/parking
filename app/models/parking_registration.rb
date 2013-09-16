d7b4391ff30ab50b0f482c08146e5ce9fc28b0bb
Carl Schwope <schwope.carl@gmail.com>
2013-09-16 10:26:58 -0400

    wip of registering a spot

--- app/models/parking_registration.rb
+++ app/models/parking_registration.rb
@@ -2,8 +2,14 @@ class ParkingRegistration < ActiveRecord::Base
 
   validates_presence_of :first_name
   validates_presence_of :last_name
-  validates_presence_of :email
-  validates_presence_of :spot_number
   validates_presence_of :parked_on
+
+  validates_format_of :email, with: /[A-Z0-9.&+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
+
+  validates_numericality_of :spot_number,
+    only_integer: true,
+    greater_than: 0,
+    less_than_or_equal_to: 60
+
 
 end
